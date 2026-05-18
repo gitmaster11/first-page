d04f8d8d464a2648086b1c5c3d64f9ab17463ebb
gitmaster11 <bekmurodsaminov7@gmail.com>
2026-05-18 22:07:11 +0500

    done

--- Football_news/news_1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class News1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news_1'
--- ImageBot/button.py
+++
@@ -1,9 +0,0 @@
-from telegram import ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup
-
-
-def admin_button():
-    button = [
-        ['Reklama 📱','Statistika 🧮'],
-        ['Javob ➡️','Asosiy qism']
-    ]
-    return ReplyKeyboardMarkup(button,resize_keyboard = True,one_time_keyboard = False)
